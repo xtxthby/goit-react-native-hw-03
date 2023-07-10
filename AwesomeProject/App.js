23ab2a5037e5592fc96b0f5faec2850f6263ae1a
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-07-11 00:49:05 +0300

    add files

---
+++ AwesomeProject/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Screens/AuthPages/LoginScreen';
+// import RegistrationScreen from './Screens/AuthPages/RegistrationScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <LoginScreen />
+      {/* <RegistrationScreen /> */}
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
